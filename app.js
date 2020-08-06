5941b6e53942fdf6c1885acef5d5ec44a2796ee8
Sonam Ramchand <sonam.ramchand@royalcyber.com>
2020-08-06 10:27:20 -0700

    Module 3 soultion change

--- app.js
+++ app.js
@@ -25,16 +25,6 @@ function FoundListDirective() {
 function FoundListDirectiveController() {
   var list = this;
 
-  // list.cookiesInList = function () {
-  //   for (var i = 0; i < list.items.length; i++) {
-  //     var name = list.items[i].name;
-  //     if (name.toLowerCase().indexOf("cookie") !== -1) {
-  //       return true;
-  //     }
-  //   }
-  //
-  //   return false;
-  // };
 }
 
 
